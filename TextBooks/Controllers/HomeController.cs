9c5928cd990e577e7eff8126bfb99b64e1a7602d
Andy Cooper <explode.90@gmail.com>
2015-08-02 15:23:56 +1000

    Added 'Books' SQL Table Added Books Controller and View based on Books Table Modified Books/Index.cshtml and BooksController.cs to include basic search form. Added some test data to Books Table.

--- TextBooks/Controllers/HomeController.cs
+++ TextBooks/Controllers/HomeController.cs
@@ -4,12 +4,48 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using System.Data;
+using System.Data.SqlClient;
+
 namespace TextBooks.Controllers
 {
     public class HomeController : Controller
     {
+
+        IFB299Entities dbcontext = new IFB299Entities();
+
         public ActionResult Index()
         {
+            test timmy = (from tbl in dbcontext.tests
+                                where tbl.FirstName=="Taylor"
+                               select tbl).FirstOrDefault();
+            timmy.LastName = "Stoneham";
+
+            dbcontext.SaveChanges();
+
+
+            
+                int p = 5;
+
+
+            dbcontext.Database.Connection.Open();
+
+            ////dbcontext.Database.ExecuteSqlCommand("INSERT INTO test VALUES(3, 'Test', 'Name');");
+
+            //String connectionString = dbcontext.Database.Connection.ConnectionString.ToString();
+            //SqlCommand cmd = new SqlCommand();
+            //cmd.Connection.ConnectionString = connectionString;
+            //cmd.CommandText = "SELECT * FROM test";
+            //cmd.CommandType = CommandType.Text;
+
+            //SqlDataReader reader;
+            //reader = cmd.ExecuteReader();
+
+            //String result = reader.GetString(1); // get column 1
+
+
+            dbcontext.Database.Connection.Close();
+            
             return View();
         }
 
---
+++ TextBooks/Controllers/testsController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TextBooks;
+
+namespace TextBooks.Controllers
+{
+    public class testsController : Controller
+    {
+        private IFB299Entities db = new IFB299Entities();
+
+        // GET: tests
+        public ActionResult Index(string name)
+        {
+            if (name != "")
+            {
+                test result = (from tbl in db.tests
+                               where tbl.FirstName == name
+                               select tbl).FirstOrDefault();
+
+                if (result != null)
+                {
+                    return View(result);
+                }
+            }
+            return View(new test());
+
+        }
+        //post index/name
+       
+        // GET: tests/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            test test = db.tests.Find(id);
+            if (test == null)
+            {
+                return HttpNotFound();
+            }
+            return View(test);
+        }
+
+        // GET: tests/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: tests/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "P_id,FirstName,LastName")] test test)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tests.Add(test);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(test);
+        }
+
+        // GET: tests/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            test test = db.tests.Find(id);
+            if (test == null)
+            {
+                return HttpNotFound();
+            }
+            return View(test);
+        }
+
+        // POST: tests/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "P_id,FirstName,LastName")] test test)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(test).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(test);
+        }
+
+        // GET: tests/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            test test = db.tests.Find(id);
+            if (test == null)
+            {
+                return HttpNotFound();
+            }
+            return View(test);
+        }
+
+        // POST: tests/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            test test = db.tests.Find(id);
+            db.tests.Remove(test);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
