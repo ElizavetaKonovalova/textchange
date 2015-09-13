48d138cec61d897c53bf4d2550d7fc77ca54518e
lizaKo <lisakonovalova@gmail.com>
2015-09-13 18:21:50 +1000

    Completed password reset

--- TextBooks/Controllers/AccountController.cs
+++ TextBooks/Controllers/AccountController.cs
@@ -242,7 +242,7 @@ namespace TextBooks.Controllers
             {
 
 
-                var body = "Hi "+ firstName + ",\nPlease confirm your account by clicking <a href =\"" + callbackURL + "\">here</a>.";
+                var body = "Hi " + firstName + ",\nPlease confirm your account by clicking <a href =\"" + callbackURL + "\">here</a>.";
                 var message = new MailMessage();
                 message.To.Add(new MailAddress(email, firstName));  // replace with valid value 
                 message.From = new MailAddress("ifb299books@gmail.com", "noreply");  // replace with valid value
@@ -260,12 +260,36 @@ namespace TextBooks.Controllers
                 smtpClient.Send(message);
             }
             catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void SendResetPasswordEmail(string code, string callbackURL, string email)
+        {
+
+            var user = db.AspNetUsers.Where(x => x.Email == email).Select(x => x.FirstName).First().ToString();
+            // SENDGRID - waiting for provisioning. Appears to work on Azure as well as locally, but emails not sent until provisioning is complete.
+            try
+            {
+                var body = "Hello " +user + ", click the link to reset your password <a href =\"" + callbackURL + "\">here</a>.";
+                var message = new MailMessage();
+                message.To.Add(new MailAddress(email));  // replace with valid value 
+                message.From = new MailAddress("ifb299books@gmail.com", "noreply");  // replace with valid value
+                message.Subject = "Verify Texchange Account";
+                message.Body = string.Format(body, "noreply", "ifb299books@gmail.com", message);
+                message.IsBodyHtml = true;
+
+                // Init SmtpClient and send
+                SmtpClient smtpClient = new SmtpClient("smtp.sendgrid.net", Convert.ToInt32(587));
+                NetworkCredential credentials = new NetworkCredential("ifb299", "IFB299Password");
+                smtpClient.Credentials = credentials;
+                smtpClient.Send(message);
+            }
+            catch (Exception ex)
                 {
                 Console.WriteLine(ex.Message);
             }
-
-
-
 
             // GMAIL (WILL NOT WORK ON AZURE, only for local testing.)
             //var body = "Please confirm your account by clicking <a href =\"" + callbackURL + "\">here</a>.";
@@ -330,19 +354,28 @@ namespace TextBooks.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = await UserManager.FindByEmailAsync(model.Email);
-                if (user == null || !(await UserManager.IsEmailConfirmedAsync(user.Id)))
+                if (model.Email.Contains("connect.qut.edu.au") || !model.Email.Equals("ifb299books@gmail.com"))
                 {
-                    // Don't reveal that the user does not exist or is not confirmed
-                    return View("ForgotPasswordConfirmation");
-                }
+                    var user = await UserManager.FindByEmailAsync(model.Email);
+                    if (user == null || !(await UserManager.IsEmailConfirmedAsync(user.Id)))
+                    {
+                        // Don't reveal that the user does not exist or is not confirmed
+                        return View("ForgotPasswordConfirmation");
+                    }
 
-                // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
-                // Send an email with this link
-                string code = await UserManager.GeneratePasswordResetTokenAsync(user.Id);
-                var callbackUrl = Url.Action("ResetPassword", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
-                await UserManager.SendEmailAsync(user.Id, "Reset Password", "Please reset your password by clicking <a href=\"" + callbackUrl + "\">here</a>");
-                return RedirectToAction("ForgotPasswordConfirmation", "Account");
+                    // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
+                    // Send an email with this link
+                    string code = await UserManager.GeneratePasswordResetTokenAsync(user.Id);
+                    var callbackUrl = Url.Action("ResetPassword", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
+                    //await UserManager.SendEmailAsync(user.Id, "Reset Password", "Please reset your password by clicking <a href=\"" + callbackUrl + "\">here</a>");
+                    SendResetPasswordEmail(code, callbackUrl, user.Email);
+
+                    return RedirectToAction("ForgotPasswordConfirmation", "Account");
+                }
+                else 
+                {
+                    AddErrors("You have typed the wrong email! Use @connect.qut.edu.au");
+                }
             }
 
             // If we got this far, something failed, redisplay form
@@ -382,7 +415,8 @@ namespace TextBooks.Controllers
                 // Don't reveal that the user does not exist
                 return RedirectToAction("ResetPasswordConfirmation", "Account");
             }
-            var result = await UserManager.ResetPasswordAsync(user.UserName, model.Code, model.Password);
+
+            var result = await UserManager.ResetPasswordAsync(user.Id, model.Code, model.Password);
             if (result.Succeeded)
             {
                 return RedirectToAction("ResetPasswordConfirmation", "Account");
