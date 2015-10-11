c694d5d40b4827f0af24cccb501df77ca0ac3050
Eric Briese <ebrie3@gmail.com>
2015-10-11 15:32:35 +1000

    Removed action results for about and contact page so that they can't be accessed.

--- TextBooks/Controllers/HomeController.cs
+++ TextBooks/Controllers/HomeController.cs
@@ -19,26 +19,12 @@ namespace TextBooks.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
 
         public ActionResult Terms()
         {
             ViewBag.Message = "Terms and Conditions";
 
             return View();
-        }
-
-        public ActionResult Contact()
-        {
-            return Redirect("mailto:ifb299books@gmail.com?subject=Support%20request");
-            //ViewBag.Message = "Your contact page.";
-
-            //return View();
         }
     }
 }
