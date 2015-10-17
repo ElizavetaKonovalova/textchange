34995b97c4664cef3d45070e9bc727dfa8fa4974
Andy Cooper <explode.90@gmail.com>
2015-10-17 15:09:15 +1000

    Tidied up autocomplete scripts and methods relating to /Books/Create

--- TextBooks/Controllers/BooksController.cs
+++ TextBooks/Controllers/BooksController.cs
@@ -4,10 +4,8 @@ using System.Data;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
-using System.Web;
 using System.Web.Mvc;
 using TextBooks.App_Start;
-using TextBooks;
 using TextBooks.Models;
 using System.Security.Claims;
 
@@ -98,56 +96,90 @@ namespace TextBooks.Controllers
             return View();
         }
 
-        // Query the GoogleBooksAPI for 
-        public ActionResult GoogleBooksQuery(string term)
+        // Get list of books for Autocomplete of Create Books view
+        public ActionResult GetAutocompleteList(string term)
         {
+            // Check search term exists
             if (term == "" || term == " ") return null;
+            // Query the Google Books API for possible matches
+            var queryResult = GoogleBooksQuery(term);
+            // Get relevant data from query result
+            var dataLists = ExtractBookData(queryResult);
+            // Format data as Json object and return
+            return Json(dataLists, JsonRequestBehavior.AllowGet);
+        }
 
-            var booksService = new Google.Apis.Books.v1.BooksService();
-            
-            var query = booksService.Volumes.List(term);
-            query.LangRestrict = "en";
-            query.MaxResults = 10;
-            var results = query.Execute();
+        // Extract required data from Google Books API Volumes data structure
+        List<string>[] ExtractBookData(Google.Apis.Books.v1.Data.Volumes volumes)
+        {
+            // Check volumes object is valid
+            if (volumes == null || volumes.Items.Count == 0) return null;
 
-            // TODO Check for errors (here?)
-            // if results == null or results.Items == null
-
-            // Get required details
+            // Setup lists (we're going to package data into a Json object)
             var titlesList = new List<string>();
             var authorsList = new List<string>();
             var isbnList = new List<string>();
             var yearList = new List<string>();
 
+            // For each book in the query result, add details to the data lists
             int i = 0;
-            while(i < results.Items.Count && titlesList.Count < 5)
+            while (i < volumes.Items.Count && titlesList.Count < 5)
             {
-                if ((results.Items[i].VolumeInfo.Title != null)
-                    && (results.Items[i].VolumeInfo.Authors != null)
-                    && (results.Items[i].VolumeInfo.IndustryIdentifiers != null)
-                    && (results.Items[i].VolumeInfo.PublishedDate != null))
+                // Check required fields are included in record
+                if ((volumes.Items[i].VolumeInfo.Title != null)
+                    && (volumes.Items[i].VolumeInfo.Authors != null)
+                    && (volumes.Items[i].VolumeInfo.IndustryIdentifiers != null)
+                    && (volumes.Items[i].VolumeInfo.PublishedDate != null)
+                    // and that we this isn't a duplicate record
+                    && (!titlesList.Contains(volumes.Items[i].VolumeInfo.Title)))
                 {
-                    if (!titlesList.Contains(results.Items[i].VolumeInfo.Title))
+                    // Add the data to the prepared lists
+                    titlesList.Add(volumes.Items[i].VolumeInfo.Title);
+                    isbnList.Add(volumes.Items[i].VolumeInfo.IndustryIdentifiers.FirstOrDefault().Identifier);
+                    yearList.Add(volumes.Items[i].VolumeInfo.PublishedDate.Substring(0, 4));
+                    authorsList.Add(volumes.Items[i].VolumeInfo.Authors[0]);
+                    
+                    // Handle 2 or 3 authors
+                    if (volumes.Items[i].VolumeInfo.Authors.Count > 1)
                     {
-                        titlesList.Add(results.Items[i].VolumeInfo.Title);
-                        authorsList.Add(results.Items[i].VolumeInfo.Authors[0]);
-                        if (results.Items[i].VolumeInfo.Authors.Count > 1)
-                        {
-                            authorsList[authorsList.Count-1] += (", " + (results.Items[i].VolumeInfo.Authors[1]));
-                        }
-                        if (results.Items[i].VolumeInfo.Authors.Count > 2)
-                        {
-                            authorsList[authorsList.Count - 1] += (", " + (results.Items[i].VolumeInfo.Authors[2]));
-                        }
-                        isbnList.Add(results.Items[i].VolumeInfo.IndustryIdentifiers.FirstOrDefault().Identifier);
-                        yearList.Add(results.Items[i].VolumeInfo.PublishedDate.Substring(0, 4));
+                        authorsList[authorsList.Count - 1] += (", " + (volumes.Items[i].VolumeInfo.Authors[1]));
+                    }
+                    if (volumes.Items[i].VolumeInfo.Authors.Count > 2)
+                    {
+                        authorsList[authorsList.Count - 1] += (", " + (volumes.Items[i].VolumeInfo.Authors[2]));
                     }
                 }
                 i++;
             }
 
+            // Group the lists into another list for Json encoding
             List<string>[] resultsObj = { titlesList, authorsList, isbnList, yearList };
-            return Json(resultsObj, JsonRequestBehavior.AllowGet);
+            return resultsObj;
+        }
+
+        // Query the GoogleBooksAPI for volumes relating to searchTerm
+        Google.Apis.Books.v1.Data.Volumes GoogleBooksQuery(string searchTerm)
+        {
+            // Create a service object for the Google Books API
+            var booksService = new Google.Apis.Books.v1.BooksService();
+
+            // Create a query object (API auth has been provided from the Google apps side. 
+            // Server set up for 2000 queries per day. If we want to exceed that, we need to
+            // implement OAuth2.0 here.)
+            var query = booksService.Volumes.List(searchTerm);
+
+            // Restric to books in English
+            query.LangRestrict = "en";
+
+            // We don't need more than 10 results per query, this speeds up response time.
+            query.MaxResults = 10;
+
+            // Execute the query and record the results for user later. Also check for failure.
+            var results = query.Execute();
+            if (results == null || results.Items == null) return null;
+
+            // Return results
+            return results;
         }
 
 
